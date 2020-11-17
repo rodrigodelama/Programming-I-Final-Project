e631954e5c7fe46490af581941cc66361d340887
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2020-11-17 15:54:05 +0100

    Initial commit

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
  This is the final project of my course Programming I at uc3m
 
  Rodrigo De Lama
+
+
+ Presentation of the final project:
+
+ Tic-Tac-Toe
--- src/mainProgram.java
+++ src/mainProgram.java
@@ -2,10 +2,24 @@
 //100451775@alumnos.uc3m.es
 //@RDLF11
 
+//Scanner imported to read user keyboard inputs
+import java.util.Scanner;
+
+//We're going for CPU mode bois - gonna get a 100%
+
 public class mainProgram {
 
+    //Declared static Scanner for permament usage, avoiding a resouse leak (if not closed)
+    static Scanner input = new Scanner(System.in);
+
+    public static void ticTacToe() {
+
+    }
+
     public static void main(String[] args) {
-        
+        //ticTacToe();
+        //runCount();
+        //reRun();
     }
 
 }
